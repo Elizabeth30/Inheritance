d7c083177169b7e5b05f6783e7a40d5a8ad9d9b5
Elizabeth <ElizabethSanchez@crbc.net>
2015-05-14 09:19:42 -0400

    Initial Commit

---
+++ Inheritance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Inheritance
+//
+//  Created by Liz Sanchez on 5/14/15.
+//  Copyright (c) 2015 Liz Sanchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
